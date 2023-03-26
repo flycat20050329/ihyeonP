bed0eb3287768e781a4ca80aaf1ea3e5dc9a4a82
dbrudals <rudals20060@gmail.com>
2023-03-26 12:27:07 +0900

    First Commit

---
+++ IhyeonP/src/test/java/com/ykm/portfolio/IhyeonPApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ykm.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IhyeonPApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
